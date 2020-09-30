d0b5f7cb67000b945fac819d5126fe4c1fcd8fa7
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-09-30 11:35:26 -0600

    added slug attribute and slugify lifecycle hook

--- api/technology/models/technology.js
+++ api/technology/models/technology.js
@@ -1,8 +1,19 @@
 'use strict';
-
+const slugify = require('slugify')
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      data.slug = slugify(data.name, { lower: true });
+    },
+  },
+};
