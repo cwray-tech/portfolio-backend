21fad4d4095f763db1e514fd9f5ff01d76bdf061
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-30 10:03:31 -0700

    Added sort by default published_at to post control

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,30 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const orderBy = require("lodash/orderBy");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      if (!ctx.query._sort) {
+        entities = orderBy(
+          await strapi.services.post.find(ctx.query),
+          "published_at",
+          "desc"
+        );
+      } else {
+        entities = await strapi.services.post.find(ctx.query);
+      }
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.post })
+    );
+  },
+};
