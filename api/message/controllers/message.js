b543dab2ea48aa9340e9a311663e434823ad85b7
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-11-15 01:01:28 -0700

    added email to contact form

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -1,8 +1,38 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.message.create(data, { files });
+    } else {
+      entity = await strapi.services.message.create(ctx.request.body);
+    }
+    entity = sanitizeEntity(entity, { model: strapi.models.message });
+    await strapi.plugins["email"].services.email.send({
+      to: "chris@solmediaco.com",
+      replyTo: `${entity.email}`,
+      subject: "New Contact Form Submission on Personal Portfolio",
+      text: `
+        The contact form on the personal portfolio website was submitted.
+        Message: ${entity.message}
+
+        Name: ${entity.name}
+
+        Email: ${entity.email}
+
+        Phone: ${entity.phone}
+      `,
+    });
+
+    return entity;
+  },
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,20 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'cloudinary',
+    provider: "cloudinary",
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+  },
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY"),
+    },
+    settings: {
+      defaultFrom: "chris@solmediaco.com",
+      defaultReplyTo: "chris@solmediaco.com",
     },
   },
 });
